5ede360cc07da23fa534688c47a3946fe67fa11e
woomin33 <woomin013@gmail.com>
2024-11-23 23:34:39 +0900

    feat: 데이터 supabase DB에 삽입하기

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function Page() {
-  return <div>Hello</div>;
+  return (
+    <div className="container flex flex-col md:items-center md:w-[720px]">
+      <div className="grid grid-cols-2 w-full gap-4 md:gap-24">
+        <div className="flex flex-col items-center">Hello</div>
+        <div className="flex flex-col items-center">Hello</div>
+      </div>
+    </div>
+  )
 }
 
 export default Page;
